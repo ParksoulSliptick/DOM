d8c52836c2da39cfdb2ed74e621a6823afa0586c
ParksoulSliptick <163039921+ParksoulSliptick@users.noreply.github.com>
2025-02-10 17:04:17 -0600

    "Funcionalidad de agregar tarea"

--- DOM/11-Ejercicio-Lista-Tareas/script.js
+++ DOM/11-Ejercicio-Lista-Tareas/script.js
@@ -2,6 +2,7 @@
 
 const taskInput = document.getElementById('taskInput');
 const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
 
 //Escuchador 
 
@@ -11,10 +12,33 @@ addButton.addEventListener('click', () => {
 
 function createTask () {
     if((taskInput.value = true)){
+
     console.log(taskInput.value);
+
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('task');
+
+    const taskText = document.createElement ('p');
+    taskText.innerText = taskInput.value;
+
+    const taskIcons = document.createElement ('div');
+    taskIcons.classList.add('task-icons');
+
+    const iconCheck = document.createElement('i');
+    iconCheck.classList.add('bi', 'bi-check', 'icon-complete');
+
+    const iconDelete = document.createElement('i');
+    iconDelete.classList.add('bi', 'bi-trash', 'icon-delete')
+
+    taskIcons.append(iconCheck, iconDelete);
+    taskItem.append(taskText, taskIcons);
+    taskList.append(taskItem);
+
     taskInput.value = '';
     } else {
       alert('Epa ahi si le entendi');
     }
 }
 
+const taskItem = document.createElement('div');
+taskItem.classList.add('task');
